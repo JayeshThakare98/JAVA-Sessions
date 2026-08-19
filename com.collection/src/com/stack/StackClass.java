f8fa3b89521ebba4e5939b7880a7e631e511c86f
Jayesh Thakare <jayesht6898@gmail.com>
2026-08-19 18:10:17 +0530

    Stack files

---
+++ com.collection/src/com/stack/StackClass.java
@@ -0,0 +1,32 @@
+package com.stack;
+
+import java.util.Stack;
+
+public class StackClass {
+	public static void main(String[] args) {
+		Stack<String> subStack = new Stack<>(); 
+		subStack.push("mobile computing"); 
+		subStack.push("Maths"); 
+		subStack.push("java"); 
+		subStack.push("sanskrit");
+		subStack.push("Marathi");
+		
+		subStack.pop();
+		subStack.pop();
+		for(String a : subStack ) {
+			System.out.println(a);
+		}
+	}
+/*
+ * Quick recap :-  vectors are as a String 
+ * Array List - > ArrayList <String> al = new ArrayList<>(); 
+ * Linklist ->  LinkList <String> lL = new LinkList<>();
+ * Stack -> Stack <String> stk = new Stack<>() ;  
+ * 
+ * Advance data manupalation 
+ * List <String> list1 = new  ArrayList<>();
+ * List <String> list2 = new LinkList<>();
+ * List <String> list3 = new Stack<>() ; 
+ * 
+ */
+}
